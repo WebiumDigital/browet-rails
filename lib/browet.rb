52848896c31907663d3249949123b4ff5a73dab8
Dmitry Yurusov <dyurusov@gmail.com>
2015-01-29 00:55:58 +0300

    add initializer

--- lib/browet.rb
+++ lib/browet.rb
@@ -1,2 +1,18 @@
 module Browet
+
+  module Config
+    extend self
+    attr_accessor :url, :account, :key
+  end
+
+  extend self
+
+  def self.init(&block)
+    if block_given?
+      block.call(Config)
+    else
+      require 'browet/initialaizer'
+    end
+  end
+
 end
---
+++ lib/browet/initializer.rb
@@ -0,0 +1,13 @@
+# Browet Global Configuration
+Browet.init do |config|
+
+  # Browet API URL
+  config.url = 'http://browet.com/api/v1'
+
+  # Browet API account
+  config.account = 'your account'
+
+  # Browet API key
+  config.key = 'your key'
+
+end
