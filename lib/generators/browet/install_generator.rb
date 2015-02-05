72b812d2a3127547363cab46a764b21f8404ce1e
Dmitry Yurusov <dyurusov@gmail.com>
2015-02-06 00:21:40 +0300

    modify generator

--- lib/generators/browet/install_generator.rb
+++ lib/generators/browet/install_generator.rb
@@ -1,9 +1,11 @@
-class InstallGenerator < Rails::Generators::Base
-  desc "Installs Browet plugin"
+module Browet
+  class InstallGenerator < Rails::Generators::Base
+    desc "Installs Browet plugin"
 
-  source_root File.expand_path('../../../browet', __FILE__)
+    source_root File.expand_path('../../../browet', __FILE__)
 
-  def create_initializer
-    copy_file 'initializer.rb', 'config/initializers/browet.rb'
+    def create_initializer
+      copy_file 'initializer.rb', 'config/initializers/browet.rb'
+    end
   end
 end
