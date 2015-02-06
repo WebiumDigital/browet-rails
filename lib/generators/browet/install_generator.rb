8e438ee8f3d3efc82aade95e97f32b4b199bf103
Dmitry Yurusov <dyurusov@gmail.com>
2015-02-06 03:11:04 +0300

    modify generator

--- lib/generators/browet/install_generator.rb
+++ lib/generators/browet/install_generator.rb
@@ -9,11 +9,11 @@ module Browet
 
     source_root File.expand_path('../../../browet', __FILE__)
 
-    def create_initializer
+    def install_initializer
       copy_file 'initializer.rb', 'config/initializers/browet.rb'
     end
 
-    def create_migration
+    def install_migration
       migration_template 'migration.rb', 'db/migrate/create_browet_cache.rb'
     end
   end
