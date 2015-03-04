99dfb439a5f3da741a8a0bf5af167a01b37985a1
Dmitry Yurusov <dyurusov@gmail.com>
2015-03-04 21:26:58 +0300

    separate main js functions

--- lib/browet/widget.rb
+++ lib/browet/widget.rb
@@ -4,80 +4,17 @@ module Browet
 
     extend self
 
-    def product_search_autocomplete(selectCallback, enterCallback, css_class = 'browet-widget-autocomplite-search-product')
-      # "<input class=\"#{css_class}\">
-      # </input>
-      # <option value=\"\" selected=\"selected\"></option>
+    def product_search_autocomplete(select_callback, enter_callback, render_callback = nil, css_class = 'browet-widget-product-search-autocomplite')
       raw "<select class=\"#{css_class}\">
-      </select>
-      <script type=\"text/javascript\">
-        var EMPTY_IDENTITY = 0;
-        var selectize = jQuery('.#{css_class}').selectize({
-          plugins: ['restore_on_backspace'],
-          createOnBlur: true,
-          create: function(input, callback) {
-            callback({title:input, #{Config.identity}:0});
-          },
-          persist: false,
-          allowEmptyOption: true,
-          valueField: '#{Config.identity}',
-          labelField: 'title',
-          searchField: ['title', 'mpn'],
-          render: {
-            option: function(item, escape) {
-              return ('id' in item) ? '<div class=\"item\"><div class=\"title\">' + escape(item.title) + 
-                '</div><div class=\"mpn\">' + escape(item.mpn) + '</div></div>' : '';
-            },
-            option_create: function(item, escape) {
-              return '';
-            }
-          },
-          load: function(query, callback) {
-            // console.log('load: ' + query);
-            // selectize.close();
-            // if (!query.length) return callback();
-            if (query.length >= #{Config.product_search_autocomplete_length})
-              $.ajax({
-                url: '/browet/#{Config.product_search_autocomplete_url}',
-                type: 'GET',
-                data: {
-                    query: query,
-                },
-                error: function() {
-                    callback();
-                },
-                success: function(res) {
-                    callback(res);
-                }
-              });
-            else {
-              //for (var i = 0; i < element[0].selectize.items.length; i++)
-              //  element[0].selectize.removeItem(value, true);
-              //element[0].selectize.refreshItems();
-              selectize.clearOptions();
-            }
-          },
-          onChange: function(slug_or_id) {
-            if (slug_or_id != EMPTY_IDENTITY)
-              #{selectCallback}(slug_or_id);
-          },
-          onEnterKeyPress: function(value) {
-            #{enterCallback}(value);
-          }
-        })[0].selectize;
-        var onKeyDown = selectize.onKeyDown.bind(selectize);
-        selectize.onKeyDown = function(e) {
-          var result = onKeyDown(e);
-          if(e.keyCode == 13)  {
-            //this.settings.onEnterKeyPress(this.items[0]);
-            selectize.blur(); // to create entered nonexistant option
-            if ( (this.items.length > 0) && (this.options[this.items[0]].#{Config.identity} == EMPTY_IDENTITY) )
-              this.settings.onEnterKeyPress(this.options[this.items[0]].title);
-          }
-          return result;
-        }.bind(selectize);
-      </script>
-      "
+        </select>
+        <script type=\"text/javascript\">
+          browetProductSearchAutocomplete(
+            '#{css_class}', #{Config.product_search_autocomplete_length},
+            '/browet/#{Config.product_search_autocomplete_url}',
+            '#{Config.identity}',
+            #{render_callback.nil? ? 'undefined' : render_callback}, 
+            #{select_callback}, #{enter_callback});
+        </script>"
     end
 
   end
